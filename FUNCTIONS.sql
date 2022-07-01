65be9eb15e1b1dc3e7af50ed73cf1a09f3e574e7
Daniel <dsaulo.92@gmail.com>
2022-07-01 11:30:15 -0300

    c15 - criação das functions

---
+++ FUNCTIONS.sql
@@ -0,0 +1,26 @@
+-- retorna uma string informando a quantidade de alunos matriculados na disciplina informada como parâmetro
+CREATE FUNCTION totalAlunos(codDisciplina INT)
+    RETURNS CHAR (50)
+    READS SQL DATA
+    RETURN (
+        SELECT CONCAT('Essa disciplina possui ', COUNT(fk_RA),' aluno(s) matriculado(s)') FROM aluno_disciplina 
+    WHERE fk_cod_disciplina = codDisciplina);
+
+-- retorna um valor inteiro com a quantidade de alunos matriculados na disciplina informada como parâmetro
+CREATE FUNCTION `qtd_alunos_disciplina`(codDisciplina INT) 
+    RETURNS INT
+    READS SQL DATA
+    RETURN(
+		SELECT COUNT(fk_RA) FROM aluno_disciplina WHERE fk_cod_disciplina = codDisciplina
+    );
+
+-- retorna uma string informando a quantidade de professores por departamento
+CREATE FUNCTION `qtdProfDepartamento`(codDepartamento INT) 
+    RETURNS char(60)
+    READS SQL DATA
+    RETURN(
+		SELECT CONCAT(d.nome_departamento, ' possui ',COUNT(p.cod_professor),' professor(es)') FROM departamento d
+		JOIN professor p
+			ON p.fk_cod_departamento = d.cod_departamento
+		WHERE cod_departamento = codDepartamento
+    );
